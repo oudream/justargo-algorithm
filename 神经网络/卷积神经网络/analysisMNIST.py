bdd87e039076c5af92f66d9b3aabba2d4c4fe5e3
oudream <oudream@126.com>
2018-09-04 10:58:50 +0800

    cnn modify
    cnn modify

--- 神经网络/卷积神经网络/analysisMNIST.py
+++ 神经网络/卷积神经网络/analysisMNIST.py
@@ -2,73 +2,63 @@
 # -*- coding: utf-8 -*-
 
 
-from PIL import Image
 import struct
+
+from PIL import Image
 
 
 def read_image(filename):
-  f = open(filename, 'rb')
+    f = open(filename, 'rb')
 
+    index = 0
+    buf = f.read()
 
-  index = 0
-  buf = f.read()
+    f.close()
 
+    magic, images, rows, columns = struct.unpack_from('>IIII', buf, index)
+    index += struct.calcsize('>IIII')
 
-  f.close()
+    for i in xrange(images):
+        # for i in xrange(2000):
+        image = Image.new('L', (columns, rows))
 
+        for x in xrange(rows):
+            for y in xrange(columns):
+                image.putpixel((y, x), int(struct.unpack_from('>B', buf, index)[0]))
+                index += struct.calcsize('>B')
 
-  magic, images, rows, columns = struct.unpack_from('>IIII' , buf , index)
-  index += struct.calcsize('>IIII')
-
-
-  for i in xrange(images):
-  #for i in xrange(2000):
-    image = Image.new('L', (columns, rows))
-
-
-    for x in xrange(rows):
-      for y in xrange(columns):
-        image.putpixel((y, x), int(struct.unpack_from('>B', buf, index)[0]))
-        index += struct.calcsize('>B')
-
-
-    print 'save ' + str(i) + 'image'
-    image.save('/media/autumn/Work/data/MNIST/mnist-png/' + str(i) + '.png')
+        print 'save ' + str(i) + 'image'
+        image.save('/eee/mnist-png/' + str(i) + '.png')
 
 
 def read_label(filename, saveFilename):
-  f = open(filename, 'rb')
-  index = 0
-  buf = f.read()
+    f = open(filename, 'rb')
+    index = 0
+    buf = f.read()
 
+    f.close()
 
-  f.close()
+    magic, labels = struct.unpack_from('>II', buf, index)
+    index += struct.calcsize('>II')
 
+    labelArr = [0] * labels
+    # labelArr = [0] * 2000
 
-  magic, labels = struct.unpack_from('>II' , buf , index)
-  index += struct.calcsize('>II')
-  
-  labelArr = [0] * labels
-  #labelArr = [0] * 2000
+    for x in xrange(labels):
+        # for x in xrange(2000):
+        labelArr[x] = int(struct.unpack_from('>B', buf, index)[0])
+        index += struct.calcsize('>B')
 
+    save = open(saveFilename, 'w')
 
-  for x in xrange(labels):
-  #for x in xrange(2000):
-    labelArr[x] = int(struct.unpack_from('>B', buf, index)[0])
-    index += struct.calcsize('>B')
+    save.write(','.join(map(lambda x: str(x), labelArr)))
+    save.write('\n')
 
-
-  save = open(saveFilename, 'w')
-
-
-  save.write(','.join(map(lambda x: str(x), labelArr)))
-  save.write('\n')
-
-
-  save.close()
-  print 'save labels success'
+    save.close()
+    print 'save labels success'
 
 
 if __name__ == '__main__':
-  read_image('/media/autumn/Work/data/MNIST/mnist/t10k-images.idx3-ubyte')
-  read_label('/media/autumn/Work/data/MNIST/mnist/t10k-labels.idx1-ubyte', '/media/autumn/Work/data/MNIST/mnist-png/label.txt')
+    read_image('/eee/mnist/t10k-images-idx3-ubyte')
+    read_label('/eee/mnist/t10k-labels-idx1-ubyte',
+               '/eee/mnist-png/label.txt')
--- 神经网络/卷积神经网络/gParam.py
+++ 神经网络/卷积神经网络/gParam.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-TOP_PATH = 'C:/Users/Administrator/Desktop/python/algorithm/dataset/t10k-images.idx3-ubyte'
-LAB_PATH = 'C:/Users/Administrator/Desktop/python/algorithm/dataset/t10k-labels.idx1-ubyte'
+TOP_PATH = '/eee/mnist/t10k-images-idx3-ubyte'
+LAB_PATH = '/eee/mnist/t10k-labels-idx1-ubyte'
 C_SIZE = 5
 F_NUM = 12
 P_SIZE = 2
